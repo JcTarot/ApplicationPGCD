3e6672d422da3b089e7ab428a2862eb63f87c948
Jc Tarot <jctarot.gep@gmail.com>
2020-01-16 14:28:44 +0100

    app version 0.2

--- src/applicationpgcd/pgcd2.java
+++ src/applicationpgcd/pgcd2.java
@@ -3,11 +3,11 @@ ALGORITHME PGCD
 VARIABLE  
 ENTIER : x, y
 DEBUT
-    AFFICHE ("un entier :")
+    AFFICHE ("Saisir un nombre entier : ")
     LIRE(x)
-    AFFICHE ("un entier :")
+    AFFICHE ("Saisir un nombre entier : ")
     LIRE(y)
-    ECRIRE ("Le PGCD de ",x,"et",y, PGCDde2EntiersPositifs(x, y))
+    ECRIRE ("PGCD : ", PGCD(x, y))
 FIN
 
 FONCTION PGCD (a,b : Entier) : Entier
@@ -23,21 +23,44 @@ FONCTION PGCD (a,b : Entier) : Entier
             FIN_SI
         FIN_TANT_QUE
      RETOURNE a
+                                 // autre
+                                TANT_QUE a*b != 0 FAIRE
+                                    SI a > b FAIRE
+                                        a <- a - b
+                                        pgcd <- b
+                                    SINON
+                                        b <- b - a
+                                    FIN_SI
+                                FIN_TANT_QUE
+                             RETOURNE a + b
   FIN
  */
 package applicationpgcd;
 
+import java.util.Scanner;
+
 /**
  *
- * @author formation_gep
+ * @author Jean-Christophe Tarot formation_gep
  */
 public class pgcd2 {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner reader = new Scanner(System.in);
+        // SAISIE
+        System.out.print("un entier : ");
+        int x = reader.nextInt();
+        System.out.print("un entier : ");
+        int y = reader.nextInt();
+        System.out.printf("pgcd : %d /n", PGCD2(x, y));
     }
     
+    public static int PGCD2(int a, int b) {
+        int pgcd=0;    
+        while ( a != b ) { 
+            if ( a >= b ) a = a-b;
+            else b = b-a;
+            }     
+        return a;
+    }
 }
